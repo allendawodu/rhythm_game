99a19bc501fd98be90bed6453696e33bd7b34f4b
Allen Dawodu <43154408+allendawodu@users.noreply.github.com>
2024-01-26 19:42:15 -0600

    Add beat sync

--- src/entities/conductor.lua
+++ src/entities/conductor.lua
@@ -1,15 +1,28 @@
 return function ()
     local conductor = {}
 
-   function conductor:load()
-            self.bpm = 0
-            self.crotchet = 0
-            
+   function conductor:load(song, bpm, offset)
+            self.song = song
+            self.bpm = bpm
+            self.crochet = 60 / self.bpm
+            self.offset = offset
+            self.songPosition = 0
+            self.lastBeat = 0
+
+            self.song:play()
 
         return self
    end
 
-   function conductor:update(dt)
+   function conductor:update()
+        self.songPosition = self.song:tell() - self.offset + self.crochet
 
+        if self.songPosition > self.lastBeat + self.crochet then
+            local error = self.songPosition - (self.lastBeat + self.crochet)
+            self.lastBeat = self.lastBeat + self.crochet - error
+            print("ding!", error)
+        end
    end
+
+   return conductor
 end
--- src/game.lua
+++ src/game.lua
@@ -2,13 +2,20 @@ local play = require "lib.play"
 
 local game = play.Scene("game")
 
+local iConductor = require "src.entities.conductor"
+local conductor
+
 function game:load()
 end
 
 function game:enter(previous, ...)
+    print("start")
+    -- conductor = iConductor():load(love.audio.newSource("dev/madtek.ogg", "stream"), 147, 0.995)
+    conductor = iConductor():load(love.audio.newSource("dev/carnation.ogg", "stream"), 118, 2.5585)
 end
 
 function game:update(dt)
+    conductor:update()
 end
 
 function game:draw()
